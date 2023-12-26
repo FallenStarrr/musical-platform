48005d656b0e23084a8b15f3e5e6833d692e4047
vladof <v.fedorchenko@shinhanfinance.kz>
2023-12-26 11:38:25 +0600

    new folder structure

---
+++ server/dist/album/album.shema.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=album.shema.js.map
---
+++ server/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import { resolve } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TrackModule } from './track/track.module';
+
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://root:root@cluster0.2v5jpdr.mongodb.net/?retryWrites=true&w=majority'),
+    FileModule,
+    ServeStaticModule.forRoot({rootPath: resolve(__dirname,  'static'),}),
+    TrackModule
+  ],
+})
+export class AppModule {}
