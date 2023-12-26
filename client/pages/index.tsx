76170739ba5d17b8422b1a646c5d5d3f1500341c
vladof <v.fedorchenko@shinhanfinance.kz>
2023-12-26 11:17:47 +0600

    front end init and Navbar component creation

---
+++ client/pages/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+        <div className='center'>
+            <h1>Добро пожаловать!</h1>
+            <h3>Здесь собраны лучшие треки!</h3>
+        </div>
+
+        <style jsx>
+            {`
+                .center {
+                    margin-top: 150px;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                }   
+               
+            `}
+        </style>
+    </>
+  )
+}
+
+export default index
