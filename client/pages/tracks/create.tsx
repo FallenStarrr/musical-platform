cf7c31ed681ccc3fb4faeef696300a3a3a8f5b09
vladof <v.fedorchenko@shinhanfinance.kz>
2023-12-26 13:06:04 +0600

    track list and track item

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,22 +1,10 @@
 import MainLayout from '@/layouts/MainLayout'
-import { Box, Button, Card, Grid } from '@mui/material'
-import { useRouter } from 'next/router'
 import React from 'react'
 
 const create = () => {
-    const router = useRouter()
   return (
     <MainLayout>
-        <Grid container justifyContent={'center'}>
-              <Card style={{width: 900}}>
-                    <Box p={3}>
-                        <Grid container justifyContent={'space-between'}>
-                            <h1>Список треков</h1>
-                            <Button onClick={() => router.push('tracks/create')}>Загрузить</Button>
-                        </Grid>
-                    </Box>        
-              </Card>  
-        </Grid>
+            <h1>Загрузка трека</h1>
     </MainLayout>
   )
 }
--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,11 +1,30 @@
 import MainLayout from '@/layouts/MainLayout'
+import { ITrack } from '@/types/track'
+import { Box, Button, Card, Grid } from '@mui/material'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const index = () => {
+  const router = useRouter()
+  const tracks: ITrack[] = [
+    {_id: '1', name: 'Трек 1', artist: 'Исполнитель 1', text: 'Текст', listens: 5, audio: 'http://localhost:5000/audio/', picture: 'http://localhost:5000/picture/', comments: []},
+    {_id: '2', name: 'Трек 2', artist: 'Исполнитель 2', text: 'Текст', listens: 5, audio: 'http://localhost:5000/audio/', picture: 'http://localhost:5000/picture/', comments: []},
+    {_id: '3', name: 'Трек 3', artist: 'Исполнитель 3', text: 'Текст', listens: 5, audio: 'http://localhost:5000/audio/', picture: 'http://localhost:5000/picture/', comments: []},
+
+  ]
   return (
     <MainLayout>
-      <div>Список треков</div>
-    </MainLayout>
+    <Grid container justifyContent={'center'}>
+          <Card style={{width: 900}}>
+                <Box p={3}>
+                    <Grid container justifyContent={'space-between'}>
+                        <h1>Список треков</h1>
+                        <Button onClick={() => router.push('/tracks/create')}>Загрузить</Button>
+                    </Grid>
+                </Box>        
+          </Card>  
+    </Grid>
+</MainLayout>
   )
 }
 
