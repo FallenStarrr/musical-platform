da543b05aa7f548fecfbdfabe55de7c76f8eab61
vladof <v.fedorchenko@shinhanfinance.kz>
2023-12-28 17:48:10 +0600

    steps page

--- client/components/StepWrapper.tsx
+++ client/components/StepWrapper.tsx
@@ -1,17 +1,31 @@
-import { Container, Stepper } from '@mui/material';
+import { Card, Container, Grid, Step, StepLabel, Stepper } from '@mui/material';
 import React, { ReactNode } from 'react'
 
 interface StepWrapperProps {
     activeStep: number;
     children: ReactNode
 }
-
+const steps = ['Инфа о треке', 'Загрузите облогу', 'Загрузите тречок']
 const StepWrapper: React.FC<StepWrapperProps> = ({activeStep, children}) => {
   return (
     <Container>
         <Stepper activeStep={activeStep}>
+              {steps.map((step, index) =>
+               <Step
+                  key={index}
+                  completed={activeStep > index}
+               >
+                  <StepLabel>{step}</StepLabel>
+               </Step>       
+              )
 
+              }
         </Stepper>
+        <Grid container justifyContent={'center'} style={{margin: '70px 0', height: 270}}>
+              <Card style={{width: 600}}>
+                {children}
+              </Card>
+        </Grid>
     </Container>
   )
 }
