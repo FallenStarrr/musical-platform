e3b45fffb2d3d1df3da716b9ae3e9f6cecfde1e1
vladof <v.fedorchenko@shinhanfinance.kz>
2024-01-03 17:58:25 +0600

    new index reducer

--- client/store/reducers/index.ts
+++ client/store/reducers/index.ts
@@ -1,10 +1,25 @@
-import { playerReducer } from './playerReduser';
-import { combineReducers } from "@/node_modules/redux/dist/redux";
+import {combineReducers} from "redux";
+import {HYDRATE} from "next-redux-wrapper";
+import {trackReducer} from "./trackReducer";
+import { playerReducer } from "./playerReduser";
 
 
-export const rootReducer = combineReducers({
-  player: playerReducer
+const rootReducer = combineReducers({
+    player: playerReducer,
+    track: trackReducer
 })
 
+export const reducer = (state, action) => {
+    if (action.type === HYDRATE) {
+        const nextState = {
+            ...state, // use previous state
+            ...action.payload, // apply delta from hydration
+        }
+        if (state.count) nextState.count = state.count // preserve count value on client side navigation
+        return nextState
+    } else {
+        return rootReducer(state, action)
+    }
+}
 
 export type RootState = ReturnType<typeof rootReducer>
